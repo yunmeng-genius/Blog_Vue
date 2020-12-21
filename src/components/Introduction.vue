98fd9fab04e3e6f13a83e0bba3eb2a244ab74e90
Daz-ddd <Daz_ddd@qq.com>
2020-12-21 19:05:06 +0800

    搭建好关于我框架 Signed-off-by: Daz-ddd <Daz_ddd@qq.com>

---
+++ src/components/Introduction.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="about">
+		<div class="blog-icon">
+			<img src="@/assets/image/more.png" alt="">
+		</div>
+		<div class="flex-box1">
+			<div class="little-image1"></div>
+			<div class="my-description">
+				<span class="large-words">你好，<br>我是{{userName}}</span>
+				<span class="small-words">欢迎来到我的世界</span>
+			</div>
+			<div class="little-image2"></div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+	@import '../assets/css/Introduction.css';
+</style>
+
+<script>
+	export default{
+		name:'Introduction',
+		data(){
+			return {
+				userName:'天才'
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
